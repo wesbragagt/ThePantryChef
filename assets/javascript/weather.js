30647b6c369596e8e7a8044ca9f9efbc08d190a5
JEverett <jonathan@jonathaneverettcreative.com>
2019-03-14 12:08:30 -0500

    Started work on pulling temperature data into the page from OpenWeatherAPI. Changed the p element on the hero image to placeholder text and added a class to identify it for JS (chef-message). Added Moment CDN to index.

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,7 +1,7 @@
 // API call
 var myAPI = "f48f376b3f3a0f2f2c42323709cb6fb9";
 
-var newQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=" + myAPI;
+var newQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=Nashville&units=imperial&appid=" + myAPI;
 
 console.log(newQueryURL);
 $.ajax({
@@ -9,6 +9,14 @@ $.ajax({
     method: "GET"
 }).then(function(response){
     console.log(response);
+
+    // Get current temperature
+    var temperature = response.main.temp;
+    temperature = Math.floor(temperature);
+    console.log(temperature);
+
+
+    
 })
 
 
